ba7299159effcdd6515bd2b3de7897440d89c11a
Orche <orkv1234@gmail.com>
2023-10-05 11:27:32 +0300

    Finalized

--- src/scripts/card.gd
+++ src/scripts/card.gd
@@ -2,17 +2,18 @@ extends Node2D
 
 @export_group('Card Props')
 @export_range(0, 13, 1) var cardNum: int # Joker is 0
-enum CardTypes {Clover, Diamond, Hurt, Spade}
-@export var type: CardTypes
+
+@export_enum('Clover', 'Diamond', 'Heart', 'Spade') var cardType: String = 'Clover'
 
 @onready var cardSprite = $Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set
+	setCardTexture()
 
 func setCardTexture():
-	var path = str('res://sprites/pcp/', type, '/card_', cardNum, '_', str(type).to_lower(), 'png') 
+	var path = str('res://sprites/pcp/', cardType, '/card_', cardNum, '_', cardType, '.png') 
+	print(path)
 	
 	cardSprite.texture = load(path)
 

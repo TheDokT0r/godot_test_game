e637b8b2334ea45f64b6a5440fda658a67f4e9c8
Orche <orkv1234@gmail.com>
2023-10-05 11:22:26 +0300

    Created sprite selector script

--- src/scripts/card.gd
+++ src/scripts/card.gd
@@ -1,16 +1,20 @@
 extends Node2D
 
-@export_group("Card Props")
+@export_group('Card Props')
 @export_range(0, 13, 1) var cardNum: int # Joker is 0
 enum CardTypes {Clover, Diamond, Hurt, Spade}
 @export var type: CardTypes
+
+@onready var cardSprite = $Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set
 
 func setCardTexture():
-	pass
+	var path = str('res://sprites/pcp/', type, '/card_', cardNum, '_', str(type).to_lower(), 'png') 
+	
+	cardSprite.texture = load(path)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):

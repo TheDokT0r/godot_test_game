d0ee8fceb4438036a25bf227a096ac4a7f8c475b
No-Fly05 <daniel05arister@gmail.com>
2023-10-07 18:58:52 +0300

    random card

--- src/scripts/card.gd
+++ src/scripts/card.gd
@@ -6,10 +6,22 @@ extends Node2D
 @export_enum('Clover', 'Diamond', 'Heart', 'Spade') var cardType: String = 'Clover'
 
 @onready var cardSprite = $Sprite2D
+@onready var mouseRegion = $MouseRegion
+
+var pickedUp: bool = false
+
+
+signal mouseRealeased
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	setCardTexture()
+	
+	mouseRegion.size.x = cardSprite.texture.get_width()
+	mouseRegion.size.y = cardSprite.texture.get_height()
+	mouseRegion.position.x =  (cardSprite.texture.get_width() / 2) * -1
+	mouseRegion.position.y =  (cardSprite.texture.get_height() / 2) * -1
+
 
 func setCardTexture():
 	var path = str('res://sprites/pcp/', cardType, '/card_', cardNum, '_', cardType, '.png') 
@@ -19,4 +31,17 @@ func setCardTexture():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if pickedUp:
+		global_position = get_global_mouse_position()
+		remove_from_group("cards")
+	
+	else :
+		add_to_group("cards")
+	
+	if Input.is_action_just_pressed("M1"):
+		mouseRealeased.emit()
+
+func _on_mouse_region_pressed():
+	pickedUp = true
+	await mouseRealeased
+	pickedUp = false
--- src/scripts/hand.gd
+++ src/scripts/hand.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var hand = $HandPos
+@onready var curve = $cardcurve
 
 var hand_start_pos
 var hand_end_pos
@@ -13,21 +14,23 @@ func _ready():
 	hand_start_pos = Vector2((hand.position.x-(hand.position.x-50)), hand.position.y)
 	hand_end_pos = Vector2((hand.position.x+(hand.position.x-50)), hand.position.y)
 	add_cards(6)
+	distribute_cards()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	distribute_cards()
 
 func _draw():
 	draw_line(hand_start_pos, hand_end_pos, Color.BLACK)
 	
 func add_cards(n):
-	print("fuck")
 	for number in n:
 		var card_inst = card.instantiate()
-		card_inst.cardType = "Diamond"
-		card_inst.cardNum = 1
+		var rng  = RandomNumberGenerator.new()
+		var type = ['Clover', 'Diamond', 'Heart', 'Spade']
+		card_inst.cardType = type[rng.randf_range(0,3)]
+		card_inst.cardNum = rng.randf_range(0,13)
 		add_child(card_inst)
 
 	distribute_cards()
@@ -36,7 +39,7 @@ func distribute_cards():
 	var cards = get_tree().get_nodes_in_group("cards")
 	var hand_ratio
 	for current_card in cards:
-		hand_ratio = float(current_card.get_index()-1)/float(cards.size()-1)
+		hand_ratio = float(current_card.get_index()-1)/float(cards.size()+1)
 		current_card.position.x = hand_end_pos.x * hand_ratio
 		current_card.position.y = hand.position.y
-		print(hand_ratio)
+		
